4bb7973cada155986fb2050b66b0da674dc58df4
slothdabski <allthatsleft169@gmail.com>
2019-08-26 12:49:29 -0500

    Bring the Havoc

--- havoc_blueline.mk
+++ havoc_blueline.mk
@@ -22,7 +22,7 @@
 #
 
 # Include DU common configuration
-include vendor/du/config/common_full_phone.mk
+include vendor/havoc/config/common.mk
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
@@ -31,7 +31,7 @@ $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-PRODUCT_NAME := du_blueline
+PRODUCT_NAME := havoc_blueline
 PRODUCT_DEVICE := blueline
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3
